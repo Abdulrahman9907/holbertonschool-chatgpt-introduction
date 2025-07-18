fe4789e8ed1da7bbbe65b2d75390ba0f7a5bb175
Abdulrahman9907 <10939@holbertonstudents.com>
2025-07-18 18:33:06 +0300

    g

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # This line was missing!
+        n -= 1  # Decrement n in each iteration
     return result
 
 f = factorial(int(sys.argv[1]))
